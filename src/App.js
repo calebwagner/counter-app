97e83db8858f35b2f060b5fd748bbf4a506387ac
Caleb Wagner <calebwagner5@gmail.com>
2022-07-17 17:55:57 -0500

    lifting the state up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
+import { Component } from 'react';
 import './App.css';
 import Counters from './components/counters';
 import NavBar from './components/navbar';
 
-function App() {
+class App extends Component{
+  state = {
+    counters:[
+    {id: 1, value: 0},
+    {id: 2, value: 2},
+    {id: 3, value: 7},
+    {id: 4, value: 4}
+    ]
+ }
+
+ handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter)
+    counters[index] = {...counter}
+    counters[index].value++;
+    this.setState({counters});
+ }
+
+ handleReset = () => {
+    const counters = this.state.counters.map(c => {
+        c.value = 0;
+        return c;
+    })
+    this.setState({counters})
+ }
+
+ handleDelete = (counterId) => {
+    const counters = this.state.counters.filter(counter => counter.id !== counterId);
+    this.setState({counters});
+ }
+
+  render() {
   return (
     <div>
-      <NavBar />
+      <NavBar
+      totalCounters={this.state.counters.filter(counter => counter.value > 0).length}
+      />
       <main className='container'>
-        <Counters />
+        <Counters
+        counters={this.state.counters}
+        onReset={this.handleReset}
+        onIncrement={this.handleIncrement}
+        onDelete={this.handleDelete}
+        />
       </main>
     </div>
   );
+}
 }
 
 export default App;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ class NarBar extends Component {
         return (
             <nav className='navbar navbar-light bg-light'>
                 <a className='navbar-brand' href='tbt'>
-                    Navbar
+                    Navbar <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
                 </a>
             </nav>
         );
