a73634711e6e8782cd04ab032d5dcd79015373a0
Caleb Wagner <calebwagner5@gmail.com>
2022-07-28 15:12:15 -0500

    destructuring arguements

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,17 +3,20 @@ import Counter from './counter';
 
 class Counters extends Component {
     render() {
+        const {onReset, counters, onDelete, onIncrement} = this.props;
+
+
         return (
         <div>
             <button
-            onClick={this.props.onReset}
+            onClick={onReset}
             className="btn btn-primary btn-sm m-2">Reset</button>
-          {this.props.counters.map(counter =>
+          {counters.map(counter =>
           <Counter
             key={counter.id}
             // bubbling event to parent & using properties from parent component
-            onDelete={this.props.onDelete}
-            onIncrement={this.props.onIncrement}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
             counter={counter}
           />
                 )}
